55b55fbd414b061214ef5ff1496df3533aaf9f43
Han-Lin-zc <hlin04@gmail.com>
2020-04-17 17:47:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Creating a chat app with angular and firebase
---
+++ src/app/feed/feed.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { ChatService } from '../services/chat.service';
+import { Observable } from 'rxjs';
+import { ChatMessage } from '../models/chat-message.model';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.css']
+})
+export class FeedComponent implements OnInit, OnChanges {
+  feed: any;
+
+  constructor(private chat: ChatService) { }
+
+  ngOnInit(): void {
+  //  this.feed = this.chat.getMessages();
+  }
+
+  ngOnChanges() {
+    // this.feed = this.chat.getMessages();
+  }
+}
